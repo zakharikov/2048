03d05e58709bdd7e51e4d0c2891700a3483118f6
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-04 18:20:44 +0300

    filled field

--- src/twothousandfortyeight/model/Field.java
+++ src/twothousandfortyeight/model/Field.java
@@ -2,4 +2,20 @@ package twothousandfortyeight.model;
 
 
 public class Field {
+
+    public int size;
+
+    public Field(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[][] createBoard(int size) {
+       int[][] i = new int[size][size];
+       return i;
+    }
+
 }
