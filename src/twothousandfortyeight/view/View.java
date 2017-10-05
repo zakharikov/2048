a126442bd44ac9dbba6b71981f2679d49bbf0450
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-05 18:53:09 +0300

    askForSize method added

--- src/twothousandfortyeight/view/View.java
+++ src/twothousandfortyeight/view/View.java
@@ -1,5 +1,21 @@
 package twothousandfortyeight.view;
 
+import java.util.Scanner;
+import twothousandfortyeight.model.Field;
+
 
 public class View {
+
+    int inputSize;
+
+    public int askForSize() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер поля:");
+        inputSize = sc.nextInt();
+        sc.close();
+        System.out.println("Вы выбрали размер поля: " + inputSize + " x " + inputSize);
+        return inputSize;
+    }
+
 }
