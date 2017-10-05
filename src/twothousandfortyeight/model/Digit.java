52999216c31fb737ba7e740e0251fd9990b59d85
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-05 17:03:15 +0300

    getDigit added

--- src/twothousandfortyeight/model/Digit.java
+++ src/twothousandfortyeight/model/Digit.java
@@ -1,7 +1,17 @@
 package twothousandfortyeight.model;
 
+import java.util.Random;
+
 
 public class Digit {
     final static int TWO = 2;
     final static int FOUR = 4;
+
+    public int getDigit(Random r) {
+        if (r.nextInt() % 9 == 0) {
+            return FOUR;
+        } else {
+            return TWO;
+        }
+    }
 }
