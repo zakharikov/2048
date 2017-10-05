714ae34b4017e785555214637dc0a429bdadd68e
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-05 18:52:26 +0300

    Main added

---
+++ src/twothousandfortyeight/Main.java
@@ -0,0 +1,14 @@
+package twothousandfortyeight;
+
+
+import twothousandfortyeight.model.Field;
+import twothousandfortyeight.view.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        View view = new View();
+        view.askForSize();
+
+    }
+}
