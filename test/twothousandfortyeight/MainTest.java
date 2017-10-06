89097d923d169223920ab247441d6c7d2c119e8d
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-06 16:22:46 +0300

    i need to rewrite createBoardTest

---
+++ test/twothousandfortyeight/MainTest.java
@@ -0,0 +1,16 @@
+package twothousandfortyeight;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sigma on 06/10/2017.
+ */
+public class MainTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
