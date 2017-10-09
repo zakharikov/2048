3465bc01798ad8dbf64b939afaac00511b9c81fa
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-09 15:54:12 +0300

    firstMove added

--- src/twothousandfortyeight/Main.java
+++ src/twothousandfortyeight/Main.java
@@ -1,20 +1,23 @@
 package twothousandfortyeight;
 
 
+import twothousandfortyeight.controller.GameController;
 import twothousandfortyeight.model.Field;
 import twothousandfortyeight.view.View;
 
 public class Main {
 
     public static void main(String[] args) {
+
         View view = new View();
         view.askForSize();
 
         Field field = new Field(view.inputSize);
-        if (field == null) {
-            System.out.println(field);
-        } else {
-            view.printBoard();
-        }
+        field.createBoard();
+        field.firstMove();
+        view.printBoard(field);
+
+//        GameController gc = new GameController();
+//        gc.firstMove(field);
     }
 }
