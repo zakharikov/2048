8a797fab1f77468ab3305ae0f56d3264493d0c28
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-09 11:44:06 +0300

    WinController created

--- src/twothousandfortyeight/controller/WinController.java
+++ src/twothousandfortyeight/controller/WinController.java
@@ -1,5 +1,18 @@
 package twothousandfortyeight.controller;
 
+import twothousandfortyeight.model.Field;
 
 public class WinController {
+    public void getWinnner (Field field) {
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < field.digitArray[i].length; j++) {
+                if (field.digitArray[i][j] == 2048) {
+                    System.out.println();
+                    System.out.println("You win! Great job!");
+                    System.out.println();
+                }
+            }
+        }
+    }
 }
