38e500616098d97270e99aca3b8832b8ccc7b79c
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-09 15:53:40 +0300

    DirectionCalculatorController created

---
+++ src/twothousandfortyeight/controller/DirectionCalculatorController.java
@@ -0,0 +1,23 @@
+package twothousandfortyeight.controller;
+
+
+import twothousandfortyeight.model.Field;
+
+public class DirectionCalculatorController {
+
+//    public int[][] upCalculator (Field field) {
+//
+//    }
+//
+//    public int[][] downCalculator (Field field) {
+//
+//    }
+//
+//    public int[][] rightCalculator (Field field) {
+//
+//    }
+//
+//    public int[][] leftCalculator (Field field) {
+//
+//    }
+}
