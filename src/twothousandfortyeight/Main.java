3d2983ec757f87aa4088973b4d56d74d1b3586cf
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-09 18:58:37 +0300

    algorythm updated

--- src/twothousandfortyeight/Main.java
+++ src/twothousandfortyeight/Main.java
@@ -5,19 +5,22 @@ import twothousandfortyeight.controller.GameController;
 import twothousandfortyeight.model.Field;
 import twothousandfortyeight.view.View;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//        Scanner sc = new Scanner(System.in);
         View view = new View();
         view.askForSize();
 
         Field field = new Field(view.inputSize);
         field.createBoard();
-        field.firstMove();
+        field.generateDigit();
         view.printBoard(field);
+        view.askForDirection();
+//        System.out.println();
 
-//        GameController gc = new GameController();
-//        gc.firstMove(field);
     }
 }
