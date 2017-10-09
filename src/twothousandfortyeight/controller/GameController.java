70e92097b587523739dba4661405a8723516fdcf
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-09 15:53:08 +0300

    GameController updated

--- src/twothousandfortyeight/controller/GameController.java
+++ src/twothousandfortyeight/controller/GameController.java
@@ -2,9 +2,13 @@ package twothousandfortyeight.controller;
 
 import twothousandfortyeight.model.Digit;
 import twothousandfortyeight.model.Field;
+import java.util.Random;
 
 
 public class GameController {
+
+
+
 
 
 }
