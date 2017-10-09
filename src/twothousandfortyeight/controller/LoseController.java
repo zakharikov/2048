0e282fb92ad9a612a2c8569d36d948b4897f9eaf
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-09 11:41:29 +0300

    LoseController created

--- src/twothousandfortyeight/controller/LoseController.java
+++ src/twothousandfortyeight/controller/LoseController.java
@@ -1,5 +1,26 @@
 package twothousandfortyeight.controller;
 
 
+import twothousandfortyeight.model.Field;
+
 public class LoseController {
+
+    public void getLooser (Field field) {
+
+        int counter = 0;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < field.digitArray[i].length; j++) {
+                if (field.digitArray[i][j] != 0 && field.digitArray[i][j] < 2048) {
+                    counter++;
+                }
+            }
+        }
+
+        if (counter == field.digitArray.length * field.digitArray.length) {
+            System.out.println();
+            System.out.println("You lose! How could anyone be so stupid?");
+            System.out.println();
+        }
+    }
 }
