49834b02d18f61e99fd552b711300c1cb50d0f2e
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-10 19:18:11 +0300

    DirectionCalculatorController created

--- src/twothousandfortyeight/controller/DirectionCalculatorController.java
+++ src/twothousandfortyeight/controller/DirectionCalculatorController.java
@@ -5,19 +5,42 @@ import twothousandfortyeight.model.Field;
 
 public class DirectionCalculatorController {
 
-//    public int[][] upCalculator (Field field) {
+    public Field rightCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = cellIndex; j >= 1; j--) {
+                if (field.digitArray[i][j] == field.digitArray[i][j - 1]) {
+                    field.digitArray[i][j] += field.digitArray[i][j - 1];
+                    field.digitArray[i][j - 1] = 0;
+                }
+            }
+        }
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = cellIndex; j >= 1; j--) {
+                while (field.digitArray[i][j] == 0) {
+                    field.digitArray[i][j] += field.digitArray[i][j - 1];
+                    field.digitArray[i][j - 1] = 0;
+                }
+            }
+        }
+
+        return field;
+    }
+
+//    public Field leftCalculator (Field field) {
 //
 //    }
 //
-//    public int[][] downCalculator (Field field) {
+//    public Field upCalculator (Field field) {
 //
 //    }
 //
-//    public int[][] rightCalculator (Field field) {
+//    public Field downCalculator (Field field) {
 //
 //    }
-//
-//    public int[][] leftCalculator (Field field) {
-//
-//    }
+
+
 }
