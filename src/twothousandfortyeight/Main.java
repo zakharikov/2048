d2541cd3e6828852814020c910661e2eb4895a21
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-10 19:18:28 +0300

    DirectionCalculatorController created

--- src/twothousandfortyeight/Main.java
+++ src/twothousandfortyeight/Main.java
@@ -4,6 +4,8 @@ package twothousandfortyeight;
 import twothousandfortyeight.controller.GameController;
 import twothousandfortyeight.model.Field;
 import twothousandfortyeight.view.View;
+import twothousandfortyeight.controller.DirectionCalculatorController;
+
 
 import java.util.Scanner;
 
@@ -12,15 +14,20 @@ public class Main {
     public static void main(String[] args) {
 
 //        Scanner sc = new Scanner(System.in);
+        DirectionCalculatorController dcc = new DirectionCalculatorController();
         View view = new View();
         view.askForSize();
 
         Field field = new Field(view.inputSize);
+
         field.createBoard();
-        field.generateDigit();
+//        field.generateDigit();
+        view.printBoard(field);
+        view.initializeBoard(field);
+        view.printBoard(field);
+        dcc.rightCalculator(field);
         view.printBoard(field);
         view.askForDirection();
-//        System.out.println();
 
     }
 }
