396ed1c6b45dd0ce90e0caaa25ae7051ef513a16
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-11 17:56:46 +0300

    DirectionCalculatorController created

--- src/twothousandfortyeight/controller/DirectionCalculatorController.java
+++ src/twothousandfortyeight/controller/DirectionCalculatorController.java
@@ -18,9 +18,20 @@ public class DirectionCalculatorController {
             }
         }
 
+        for (int x = 0; x < field.digitArray.length; x++) {
+            rightMover(field);
+        }
+
+        return field;
+    }
+
+    private Field rightMover(Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
         for(int i = 0; i < field.digitArray.length; i++) {
             for (int j = cellIndex; j >= 1; j--) {
-                while (field.digitArray[i][j] == 0) {
+                if (field.digitArray[i][j] == 0) {
                     field.digitArray[i][j] += field.digitArray[i][j - 1];
                     field.digitArray[i][j - 1] = 0;
                 }
@@ -30,17 +41,112 @@ public class DirectionCalculatorController {
         return field;
     }
 
-//    public Field leftCalculator (Field field) {
-//
-//    }
-//
-//    public Field upCalculator (Field field) {
-//
-//    }
-//
-//    public Field downCalculator (Field field) {
-//
-//    }
+    public Field leftCalculator (Field field) {
 
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < cellIndex; j++) {
+                if (field.digitArray[i][j] == field.digitArray[i][j + 1]) {
+                    field.digitArray[i][j] += field.digitArray[i][j + 1];
+                    field.digitArray[i][j + 1] = 0;
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            leftMover(field);
+        }
+
+        return field;
+    }
+
+    private Field leftMover(Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < cellIndex; j++) {
+                if (field.digitArray[i][j] == 0) {
+                    field.digitArray[i][j] += field.digitArray[i][j + 1];
+                    field.digitArray[i][j + 1] = 0;
+                }
+            }
+        }
+
+        return field;
+    }
+
+    public Field upCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < cellIndex; j++) {
+                if (field.digitArray[j][i] == field.digitArray[j + 1][i]) {
+                    field.digitArray[j][i] += field.digitArray[j + 1][i];
+                    field.digitArray[j + 1][i] = 0;
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            upMover(field);
+        }
+
+        return field;
+    }
+
+    private Field upMover(Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < cellIndex; j++) {
+                if (field.digitArray[j][i] == 0) {
+                    field.digitArray[j][i] += field.digitArray[j + 1][i];
+                    field.digitArray[j + 1][i] = 0;
+                }
+            }
+        }
+
+        return field;
+    }
+
+    public Field downCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = cellIndex; j >= 1; j--) {
+                if (field.digitArray[j][i] == field.digitArray[j - 1][i]) {
+                    field.digitArray[j][i] += field.digitArray[j - 1][i];
+                    field.digitArray[j - 1][i] = 0;
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            downMover(field);
+        }
+
+        return field;
+    }
+
+    private Field downMover(Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = cellIndex; j >= 1; j--) {
+                if (field.digitArray[j][i] == 0) {
+                    field.digitArray[j][i] += field.digitArray[j - 1][i];
+                    field.digitArray[j - 1][i] = 0;
+                }
+            }
+        }
+
+        return field;
+    }
 
 }
