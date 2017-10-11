85da5a16c17eca84a711fd22b9c0874d8c3882b6
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-11 17:56:36 +0300

    DirectionCalculatorController created

--- src/twothousandfortyeight/controller/GameController.java
+++ src/twothousandfortyeight/controller/GameController.java
@@ -1,12 +1,29 @@
 package twothousandfortyeight.controller;
 
-import twothousandfortyeight.model.Digit;
+import twothousandfortyeight.model.Score;
 import twothousandfortyeight.model.Field;
+import twothousandfortyeight.view.View;
+import twothousandfortyeight.controller.DirectionCalculatorController;
+
 import java.util.Random;
+import java.awt.event.KeyEvent;
 
 
 public class GameController {
 
+//    public Field makeMove (Field field, Score score) {
+//
+//
+//
+//    }
 
+    public void direction (View view, DirectionCalculatorController dcc, Field field) {
+        switch (view.askForDirection()) {
+            case "w": dcc.upCalculator(field);
+            case "a": dcc.leftCalculator(field);
+            case "s": dcc.downCalculator(field);
+            case "d": dcc.rightCalculator(field);
+        }
+    }
 
 }
