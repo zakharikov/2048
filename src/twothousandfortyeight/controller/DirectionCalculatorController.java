ca582e35930740e73da36cb777828f58cfdd4f01
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-13 16:39:06 +0300

    DirectionCalculatorController updated

--- src/twothousandfortyeight/controller/DirectionCalculatorController.java
+++ src/twothousandfortyeight/controller/DirectionCalculatorController.java
@@ -149,4 +149,94 @@ public class DirectionCalculatorController {
         return field;
     }
 
+    public Field altLeftCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < cellIndex; j++) {
+                for (int k = 1; k <= cellIndex - j; k++) {
+                    if (field.digitArray[i][j] == field.digitArray[i][j + k]) {
+                        field.digitArray[i][j] += field.digitArray[i][j + k];
+                        field.digitArray[i][j + k] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            leftMover(field);
+        }
+
+        return field;
+    }
+
+    public Field altUpCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = 0; j < cellIndex; j++) {
+                for (int k = 1; k <= cellIndex - j; k++) {
+                    if (field.digitArray[j][i] == field.digitArray[j + k][i]) {
+                        field.digitArray[j][i] += field.digitArray[j + k][i];
+                        field.digitArray[j + k][i] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            upMover(field);
+        }
+
+        return field;
+    }
+
+    public Field altRightCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = cellIndex; j >= 1; j--) {
+                for (int k = 1; k <= j; k++) {
+                    if (field.digitArray[i][j] == field.digitArray[i][j - k]) {
+                        field.digitArray[i][j] += field.digitArray[i][j - k];
+                        field.digitArray[i][j - k] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            rightMover(field);
+        }
+
+        return field;
+    }
+
+    public Field altDownCalculator (Field field) {
+
+        int cellIndex = field.digitArray.length - 1;
+
+        for(int i = 0; i < field.digitArray.length; i++) {
+            for (int j = cellIndex; j >= 1; j--) {
+                for (int k = 1; k <= j; k++) {
+                    if (field.digitArray[j][i] == field.digitArray[j - k][i]) {
+                        field.digitArray[j][i] += field.digitArray[j - k][i];
+                        field.digitArray[j - k][i] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < field.digitArray.length; x++) {
+            downMover(field);
+        }
+
+        return field;
+    }
+
 }
+
+
