32fd819967ec1916511aef3f3d63277a3fcf7487
zakharikov <32512880+zakharikov@users.noreply.github.com>
2017-10-23 18:29:58 +0300

    добавлены контроллеры возможности хода

--- src/twothousandfortyeight/controller/GameController.java
+++ src/twothousandfortyeight/controller/GameController.java
@@ -11,19 +11,55 @@ import java.awt.event.KeyEvent;
 
 public class GameController {
 
-//    public Field makeMove (Field field, Score score) {
-//
-//
-//
-//    }
+    public int previousMove = 0;
 
-//    public void direction (View view, DirectionCalculatorController dcc, Field field) {
-//        switch (view.askForDirection()) {
-//            case "w" : dcc.upCalculator(field);
-//            case "a" : dcc.leftCalculator(field);
-//            case "s" : dcc.downCalculator(field);
-//            case "d" : dcc.rightCalculator(field);
-//        }
-//    }
+    public Field generateAutoMove (DirectionCalculatorController dcc, Field field, View view) {
 
+        Random r = new Random();
+
+        switch (r.nextInt(4) + 1) {
+
+            case 1:
+                if (dcc.leftAbility2Move(field)) {
+                    dcc.leftCalculator(field);
+                    field.generateDigit();
+                    view.printBoard(field);
+                    break;
+                } else {
+                    generateAutoMove(dcc, field, view);
+                }
+            case 2:
+                if (dcc.upAbility2Move(field)) {
+                    dcc.upCalculator(field);
+                    field.generateDigit();
+                    view.printBoard(field);
+                    break;
+                } else {
+                    generateAutoMove(dcc, field, view);
+                }
+            case 3:
+                if (dcc.rightAbility2Move(field)) {
+                    dcc.rightCalculator(field);
+                    field.generateDigit();
+                    view.printBoard(field);
+                    break;
+                } else {
+                    generateAutoMove(dcc, field, view);
+                }
+            case 4:
+                if (dcc.downAbility2Move(field)) {
+                    dcc.downCalculator(field);
+                    field.generateDigit();
+                    view.printBoard(field);
+                    break;
+            } else {
+                generateAutoMove(dcc, field, view);
+            }
+        }
+        return field;
+    }
+    public void autoPlay(WinController wc, LoseController lc, DirectionCalculatorController field, Field dcc, View view) {
+
+
+    }
 }
